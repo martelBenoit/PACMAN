d7592ab38520d97dad70c042d69f6ab762b61164
martelBenoit <benoit.1404@gmail.com>
2019-03-11 09:50:24 +0100

    Tile Maze (1)

---
+++ src/Maze.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Maze {
+
+    private int powerTime;
+    private int fruitValue;
+    private int pillValue;
+    private int regenerationTime;
+    private int ghostSpeed;
+
+    private ArrayList<Tile> tiles;
+
+    public Maze(int powerTime, int fruitValue, int pillValue, int regenerationTime, int ghostSpeed){
+
+        this.powerTime = powerTime;
+        this.fruitValue = fruitValue;
+        this.pillValue = pillValue;
+        this.regenerationTime = regenerationTime;
+        this.ghostSpeed = ghostSpeed;
+
+    }
+
+    public int getFruitValue() {
+        return fruitValue;
+    }
+
+
+    public int getPillValue() {
+        return pillValue;
+    }
+
+
+
+    public int getRegenerationTime() {
+        return regenerationTime;
+    }
+
+
+    public int getGhostSpeed() {
+        return ghostSpeed;
+    }
+
+
+
+    public ArrayList<Tile> getTiles() {
+        return tiles;
+    }
+
+
+    public int getPowerTime() {
+        return powerTime;
+    }
+
+
+}
