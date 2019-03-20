25be4ffd9f74f9a7e1c624252ac78f618d584ef4
Yoann Le Dréan <le-drean.e1601252@etud.univ-ubs.fr>
2019-03-20 13:41:32 +0100

    Merge remote-tracking branch 'origin/master' into yoann
    # Conflicts:
    #	src/Launcher.java
    #	src/control/Controller.java
    #	src/model/Character.java
    #	src/model/Game.java
    #	src/model/Ghost.java
    #	src/model/Pacman.java
    #	src/view/GameFrame.java

---
+++ src/model/Character.java
@@ -0,0 +1,26 @@
+package model;
+
+public abstract class Character {
+
+    protected Tile tile;
+    private Direction direction;
+
+    public Tile getTile(){
+        return this.tile;
+    }
+
+    public abstract void move();
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+    
+    public void setDirection(Direction d) {
+		this.direction = d;
+		System.out.println("Changement de direction : " + d);
+	}
+	
+	public Direction getDirection() {
+		return this.direction;
+	}
+}
